6b82b5115c2e64ad3e637470437fd766e0dc45a9
rw1202 <rw120216822@gmail.com>
2018-12-05 15:10:36 +0000

    trainee getters and setters

--- TrainerAPI/src/main/java/com/qa/TrainerAPI/persistence/domain/Trainee.java
+++ TrainerAPI/src/main/java/com/qa/TrainerAPI/persistence/domain/Trainee.java
@@ -2,4 +2,27 @@ package com.qa.TrainerAPI.persistence.domain;
 
 public class Trainee {
 
+	private String firstName;
+	private String lastName;
+	private String traineeId;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getTraineeId() {
+		return traineeId;
+	}
+	public void setTraineeId(String traineeId) {
+		this.traineeId = traineeId;
+	}
+
 }
