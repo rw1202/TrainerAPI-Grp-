c6970cfb50811e5878367a333a429784a9573178
rw1202 <rw120216822@gmail.com>
2018-12-05 17:31:46 +0000

    service Test Continued

--- TrainerAPI/src/main/java/com/qa/TrainerAPI/service/business/TrainerServiceImpl.java
+++ TrainerAPI/src/main/java/com/qa/TrainerAPI/service/business/TrainerServiceImpl.java
@@ -1,5 +1,37 @@
 package com.qa.TrainerAPI.service.business;
 
-public class TrainerServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.qa.TrainerAPI.persistence.domain.Trainee;
+import com.qa.TrainerAPI.persistence.repository.TrainerRepository;
+
+@Service
+public class TrainerServiceImpl implements TrainerService{
+
+	@Autowired
+	private TrainerRepository trainerRepo;
+	
+//	@Autowired
+//	private APICaller external;
+	
+	public void setRepo(TrainerRepository persist) {
+		this.trainerRepo = persist;
+	}
+	
+	
+	
+	@Override
+	public Iterable<Trainee> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Trainee get(Long Id) {
+		return trainerRepo.findbyId(Id);
+	}
+
+	
+	
 }
