53ce2907c62a5b7d809a6630eb3648725d0993b7
Admin <Admin@STUDENT06>
2018-12-11 17:09:48 +0000

    Endpoint Test

--- TrainerAPI/src/test/java/com/qa/TrainerAPI/RestTest/EndpointTest.java
+++ TrainerAPI/src/test/java/com/qa/TrainerAPI/RestTest/EndpointTest.java
@@ -1,15 +1,21 @@
 package com.qa.TrainerAPI.RestTest;
 
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doAnswer;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.stubbing.Answer;
 import org.springframework.test.context.junit4.SpringRunner;
 import com.qa.TrainerAPI.persistence.domain.Trainer;
 import com.qa.TrainerAPI.rest.TrainerEndpoint;
@@ -18,12 +24,14 @@ import com.qa.TrainerAPI.service.business.TrainerService;
 @RunWith(SpringRunner.class)
 
 public class EndpointTest {
-
+	private Trainer trainer1 = new Trainer();
+	private ArrayList<Trainer> trainerList = new ArrayList<Trainer>();
 	@Mock
 	TrainerService service;
 
 	@InjectMocks
 	private TrainerEndpoint endpoints;
+	
 
 	@Test
 	public void endPointTests_GetAll() {
@@ -56,20 +64,23 @@ public class EndpointTest {
 	}
 	
 	@Test
-	public void endPointTests_Delete() {
-		ArrayList<Trainer> trainerList = new ArrayList<Trainer>();
-		Trainer trainer1 = new Trainer();
-		Trainer trainer2 = new Trainer();
+	public void endPointTest_addTrainer() {
+	
+		trainer1.setFirstName("");
+		trainer1.setLastName("");
+		trainer1.setUsername("");
 		
-		trainer1.setFirstName("Rob");
-		trainer1.setLastName("Last");
-		trainer1.setUsername("rob@mail.com");
 
-		trainerList.add(trainer1);
-		trainerList.add(trainer2);
-		Mockito.when(service.get("rob@mail.com")).thenReturn(Optional.of(trainer1));
-		Assert.assertEquals(endpoints.getTrainer("rob@mail.com"), Optional.of(trainer1));
+	
 		
+		Mockito.when(service.add(trainer1, "password")).thenReturn("Added");
+		assertEquals("Added",endpoints.addTrainer(trainer1, "password"));
+	}
+	
+	@Test
+	public void endPointTest_updateTrainer() {
+		Mockito.when(service.add(trainer1, "rob@mail.com")).thenReturn("Updated");
+		Assert.assertEquals("Updated", endpoints.updateTrainer(trainer1, "rob@mail.com"));
 	}
 	
 	
