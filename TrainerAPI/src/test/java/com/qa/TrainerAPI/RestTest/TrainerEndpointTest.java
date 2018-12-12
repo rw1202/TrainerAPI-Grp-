bdcf49b67fcf0efb09e6b039c9f1b9174371ba64
rw1202 <rob120216822@gmail.com>
2018-12-12 12:27:55 +0000

    refactored 75% coverage

---
+++ TrainerAPI/src/test/java/com/qa/TrainerAPI/RestTest/TrainerEndpointTest.java
@@ -0,0 +1,44 @@
+package com.qa.TrainerAPI.RestTest;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.qa.TrainerAPI.persistence.domain.Trainee;
+import com.qa.TrainerAPI.rest.TrainerEndpoint;
+import com.qa.TrainerAPI.service.business.TrainerService;
+
+public class TrainerEndpointTest {
+	@InjectMocks
+	private TrainerEndpoint trainerEndpoint;
+	
+	@Mock
+	private TrainerService service;
+	
+	@Before
+	public void init() {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void addTrainee() {
+		Trainee trainee= new Trainee();
+		trainee.setFirstName("firstname");
+		trainee.setLastName("lastname");
+		trainee.setTraineeId(111);
+		
+		Trainee traineeToBeAdded = new Trainee();
+		traineeToBeAdded.setFirstName("firstName");
+		traineeToBeAdded.setLastName("lastName");
+		traineeToBeAdded.setTraineeId(111);
+		
+		when(service.add(traineeToBeAdded)).thenReturn(trainee);
+		Trainee traineeAdded=traineeEndpoint.addTrainee(traineeToBeAdded);
+		assertEquals(traineeAdded, trainee);
+		
+		
+	}
+}
